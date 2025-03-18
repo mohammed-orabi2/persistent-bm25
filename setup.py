468df5366bd6bda2b000bf278b9f0c7000ddbb28
mohammed-orabi2 <mohamed.orabi@studentgator.com>
2025-03-18 12:45:23 +0200

    feat: update setup file for new version

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="persistent_bm25",
-    version="0.2",  
+    version="0.4",  
     author="Mohammed Orabi",
     author_email="mahmedorabi297@gmail.com",
     description="A persistent implementation of BM25 retrieval.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/persistent_bm25",
+    url="https://github.com/mohammed-orabi2/persistent-bm25",
     packages=find_packages(),
     install_requires=[
         "langchain",
@@ -21,6 +21,3 @@ setup(
     ],
     python_requires=">=3.7",
 )
-
-
-
